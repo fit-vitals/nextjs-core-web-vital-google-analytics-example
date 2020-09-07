237e07a01aff62a28adfb0d9f41c27f2538f8f8b
Drew Bredvick <dbredvick@gmail.com>
2020-09-07 17:10:09 -0500

    example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import analytics from '../util/analytics';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export function reportWebVitals({ id, name, label, value }) {
+  analytics.track(name, {
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
+
+export default MyApp;
