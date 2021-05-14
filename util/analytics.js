fefcfada9bf5c4010fea241c24a7a744c6b2453a
Drew Bredvick <dbredvick@gmail.com>
2021-05-14 09:55:50 -0500

    Update analytics.js
    update with divjoy credit & note on tracking id

--- util/analytics.js
+++ util/analytics.js
@@ -4,11 +4,13 @@ import Router from 'next/router';
 
 // Initialize analytics and plugins
 // Documentation: https://getanalytics.io
+// props to Divjoy for letting me borrow this code snippet. 
+// check out Divjoy at https://divjoy.com/?via=drew
 const analytics = Analytics({
   debug: process.env.NODE_ENV === 'production',
   plugins: [
     googleAnalyticsPlugin({
-      trackingId: 'UA-73089861-6',
+      trackingId: 'UA-73089861-6', // change this to your trackingID
     }),
   ],
 });
